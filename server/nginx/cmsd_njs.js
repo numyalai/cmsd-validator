34b014a41449fb34bf2c3b3cfefe06fb6a1e40ce
dom57432 <dominik.busse@fokus.fraunhofer.de>
2023-07-07 13:55:10 +0200

    add value randomization

--- server/nginx/cmsd_njs.js
+++ server/nginx/cmsd_njs.js
@@ -15,41 +15,88 @@ function writeLog(msg) {
 }
 
 function sendCMSDHeaders(r){
-    writeLog("in sendCMSDHeaders");
 
     let staticResponse
     let dynamicResponse
 
     let dashObjUri = r.variables.args.split('/cmsdValidator')[1].split(' ')[0];
 
-    staticResponse = 'at=' + '12345' + ','
-    staticResponse += 'br=' + '12345' + ','
-    staticResponse += 'ht=' + '12345' + ','
-    staticResponse += 'n=' + 'Intermediary Identifier' + ','
-    staticResponse += 'nor=' + 'path|' + ','
-    staticResponse += 'nrr=' + '1268-1268' + ','
-    staticResponse += 'd=' + '12345' + ','
-    staticResponse += 'ot=' + 'v' + ','
-    staticResponse += 'su=' + 'false' + ','
-    staticResponse += 'st=' + 'v' + ','
-    staticResponse += 'sf=' + '(d)' + ','
-    staticResponse += 'v=' + '12345'
+    staticResponse = 'at='   + getRandomInt() + ','
+    staticResponse += 'br='  + getRandomInt() + ','
+    staticResponse += 'ht='  + getRandomInt() + ','
+    staticResponse += 'n='   + 'Intermediary_' + getRandomString() + ','
+    staticResponse += 'nor=' + getRandomString() + '|'  + getRandomString() + '|' + getRandomString() + ','
+    staticResponse += 'nrr=' + getRandomInt() + '-' + getRandomInt() + '|' + getRandomInt() + '-' + getRandomInt() + '|' + getRandomInt() + '-' + getRandomInt() + ','
+    staticResponse += 'd='   + getRandomInt() + ','
+    staticResponse += 'ot='  + getRandomObjectType() + ','
+    staticResponse += 'su='  + getRandomBool() + ','
+    staticResponse += 'st='  + getRandomStreamingType() + ','
+    staticResponse += 'sf='  + getRandomStreamingFormat() + ','
+    staticResponse += 'v='   + getRandomInt()
 
-    dynamicResponse = '"Identifier";'
-    dynamicResponse += 'du=' + 'true' + ';'
-    dynamicResponse += 'etp=' + '12345' + ';'
-    dynamicResponse += 'mb=' + '12345' + ';'
-    dynamicResponse += 'rd=' + '12345' + ';'
-    dynamicResponse += 'rtt=' + '12345'
+    dynamicResponse = '"CDN_:' + getRandomString() + '";'
+    dynamicResponse += 'du='     + getRandomBool() + ';'
+    dynamicResponse += 'etp='    + getRandomInt() + ';'
+    dynamicResponse += 'mb='     + getRandomInt() + ';'
+    dynamicResponse += 'rd='     + getRandomInt() + ';'
+    dynamicResponse += 'rtt='    + getRandomInt()
 
 
     function done(res) {
         r.headersOut['CMSD-Dynamic'] = dynamicResponse;
-        r.headersOut['CMSD-Static'] = staticResponse
+        r.headersOut['CMSD-Static'] = staticResponse;
         r.return(res.status, res.responseBody);
     }
     r.subrequest(dashObjUri, r.variables.args, done);
     r.finish();
+}
+
+function getRandomInt(){
+    return Math.floor(Math.random() * 10000) + 1;
+}
+
+function getRandomObjectType(){
+    let types = ['m', 'a', 'v', 'av', 'i', 'c', 'tt', 'k' ,'o'];
+    let i = Math.floor(Math.random() * types.length);
+
+    return types[i];
+}
+
+function getRandomStreamingType(){
+    let types = ['v', 'l'];
+    let i = Math.floor(Math.random() * types.length);
+
+    return types[i];
+}
+
+function getRandomStreamingFormat(){
+    let formats = ['d', 'h', 's', 'o'];
+    let formatsString = '(';
+
+    for(let i = 0 ; i <= Math.floor(Math.random() * formats.length); i++){
+        let j = Math.floor(Math.random() * (formats.length));
+        formatsString += formats.splice(j, 1) + ' ';
+    }
+
+    return (formatsString.slice(0, -1)) + ')';
+}
+
+function getRandomBool(){
+    let bool = ['true', 'false'];
+    let i = Math.floor(Math.random() * bool.length);
+
+    return bool[i];
+}
+
+function getRandomString(){
+    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let string = '';
+
+    for(let i = 0; i <= Math.floor(Math.random() * (characters.length)); i++){
+        string += characters.charAt(Math.random() * (characters.length));
+    }
+    
+    return string;
 }
 
 // Note: We need to add the function to nginx.conf file too for HTTP access
